9d47b431201c25f8f97de3c48489f0aa4fed2a2a
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-10-24 23:14:29 -0600

    hace la request a la db

---
+++ src/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const sqlConnection = require('./db')
+
+app.get('/',async (req, res) => {
+    try {
+        const pool = await sqlConnection
+        const request = pool.request()
+        console.log(request)
+        const result = await request.query('select * from Ingrediente')
+        console.log(result)
+        res.json( result.recordset)
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+   }
+})
+app.listen(3000,()=> console.log('Server escuchando en el puerto 3000'))
