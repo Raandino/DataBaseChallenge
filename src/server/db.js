1d2fb350959e12514ae862b47bc3d0de4a808993
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-10-24 23:14:21 -0600

    genera un pool de conneciones a la db

---
+++ src/server/db.js
@@ -0,0 +1,17 @@
+const sql = require('mssql')
+require('dotenv').config()
+
+const dbConfig = {
+    server:  process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+}
+
+const pool = new sql.ConnectionPool(dbConfig)
+    .connect()
+    .then((pool) => {
+        return pool
+    })
+
+module.exports = pool 
