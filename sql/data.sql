70d6423670ced72dede101c014bbe7d5676d1a02
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-11-01 12:46:48 -0600

    SQL Folder

---
+++ sql/data.sql
@@ -0,0 +1,7 @@
+select * from Platillo_Ingrediente
+insert into Unidad values ('Lb')
+insert into Unidad values ('gr')
+
+insert into Inventario values('Tomate',1,2)
+insert into Platillo values('Pizza',25,0)
+INSERT into Platillo_Ingrediente values(1,1,2,25)
