13fc0b4385e7ea0c2c2b42ae48555c615e80d1a3
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-11-06 19:56:56 -0600

    Data para Unidades

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,15 @@
-select * from Platillo_Ingrediente
+-- Unidades de Masa
 insert into Unidad values ('Lb')
 insert into Unidad values ('gr')
+insert into Unidad values ('Kg')
+-- Unidades de Liquido
+insert into Unidad values ('ml')
+insert into Unidad values ('L')
+-- Unidades Volumetricas
+insert into Unidad values ('tsp')
+insert into Unidad values ('Tbsp')
+insert into Unidad values ('Taza')
+
 
 insert into Inventario values('Tomate',1,2)
 insert into Platillo values('Pizza',25,0)
