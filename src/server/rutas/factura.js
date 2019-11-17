10c84e9cdf05fb419b0b9ac41809ca5f3298333a
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-11-16 18:32:57 -0600

    Ruta para Facturas

--- src/server/rutas/factura.js
+++ src/server/rutas/factura.js
@@ -2,4 +2,26 @@ import { Router } from 'express'
 
 const factura = new Router()
 
+
+
+factura.route("/")
+    .get(async (req, res) => { //Devuelve toda las facturas con su detalle
+        
+    })
+    .post(async(req, res) => { //Inserta una nueva factura
+
+    })
+
+
+factura.route("/:id")
+    .get(async (req, res) => { // Selecciona la factura con ese Id
+        
+    })
+    .delete(async (req, res) => {// Elimina esa factura
+        
+    })
+    .post(async (req, res) => { //Actualiza la factura
+        
+    })
+
 export default factura
