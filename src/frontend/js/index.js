ad0be4ce335590a6969205b0d95c695b82892892
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-11-17 20:42:49 -0600

    Frontend
    Ya esta el set up para trabajar el front end dentro del repositorio
    para ejecutar solamente el front-end se ejecura `npm run dev:fe-watch`

---
+++ src/frontend/js/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+const rootNode = document.getElementById('root')
+
+const App = () => {
+
+    return (
+        <>
+            <h1>Hello from React</h1>
+        </>
+    )
+}
+ReactDOM.render(<App></App>, rootNode)
--- src/server/rutas/usuario.js
+++ src/server/rutas/usuario.js
@@ -2,4 +2,23 @@ import { Router } from 'express'
 
 const usuario = new Router()
 
+usuario.route("/")
+    .get(async (req, res) => {
+
+    })
+    .post(async (req, res) => {
+
+    })
+
+usuario.route("/:id")
+    .get(async (req, res) => {
+
+    })
+    .delete(async (req, res) => {
+
+    })
+    .post(async (req, res) => {
+
+    })
+    
 export default usuario
